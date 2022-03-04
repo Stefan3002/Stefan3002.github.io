899b2bde5234025e5288878cf6cccae54ad3123a
Stefan3002 <92794548+Stefan3002@users.noreply.github.com>
2022-03-04 02:10:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function TypeWriterHelper() {
+    TypeWriterHelperSkills()
     const time = 250
     let words = ["student.","WEB developer (in progress)."]
     const text = document.querySelector(".typewriter")
@@ -21,7 +22,7 @@ function TypeWriter(i,j,words,word,text,text2,time) {
     if (i <= word.length) {
         text.innerHTML += word.charAt(i)
         text2.innerHTML += word.charAt(i)
-        // console.log(i)
+        // console.zlog(i)
         setTimeout(function(){TypeWriter(i + 1,j,words,word,text,text2,time)}, time)
     }
     else
@@ -36,4 +37,57 @@ function RemoveTypeWriter(i,j,words,word,text,text2,time){
     }
     else
         TypeWriterWords(j + 1,words,text,text2,time + 150)
+}
+
+
+
+//Skills typewriter
+
+
+function TypeWriterHelperSkills() {
+    const time = 250
+    let words = ["HTML.", "CSS.", "JS.", "Bootstrap.", "SASS.", "React (in progress).", "Python.", "Node (in progress).","SQL (in progress).", "C/C++ (in progress)."]
+    const text = document.querySelector(".typewriter3")
+    let i = 0, j = 0
+    TypeWriterWordsSkills(j,words,text,time)
+}
+function TypeWriterWordsSkills(j,words,text,time){
+    // console.log(j)
+    const Elements = document.querySelectorAll(".element")
+    if(j <= words.length - 1) {
+        // console.log(Elements[j])
+        if(j)
+            Elements[j - 1].classList.toggle("element-active")
+        else
+            Elements[Elements.length - 1].classList.remove("element-active")
+
+        Elements[j].classList.toggle("element-active")
+
+        TypeWriterSkills(0,j,words, words[j], text, 250 - words[j].length * 5)
+        j++
+    }
+    else
+        TypeWriterWordsSkills(0,words,text,time)
+
+
+}
+function TypeWriterSkills(i,j,words,word,text,time) {
+    if (i <= word.length) {
+        text.innerHTML += word.charAt(i)
+        // console.zlog(i)
+        setTimeout(function(){TypeWriterSkills(i + 1,j,words,word,text,time)}, time)
+    }
+    else {
+        setTimeout(function(){RemoveTypeWriterSkills(i, j, words, word, text, time - 150)},500)
+        // RemoveTypeWriterSkills(i, j, words, word, text, time - 150)
+    }
+}
+function RemoveTypeWriterSkills(i,j,words,word,text,time){
+    if (i >= 0) {
+        text.innerHTML = text.innerHTML.slice(0,text.innerHTML.length - 1)
+        // console.log(i)
+        setTimeout(function(){RemoveTypeWriterSkills(i - 1,j,words,word,text,time)}, time)
+    }
+    else
+        setTimeout(function(){TypeWriterWordsSkills(j + 1,words,text,time + 150)},200)
 }
